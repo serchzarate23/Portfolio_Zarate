81b51f0ef3bfadf3c86d3cf9c06ac255ebac591b
Sergio <sergiozarate1991@hotmail.com>
2022-10-10 14:55:43 -0300

    Agregue Nuevos Datos
    Para crear una persona, editarla, borrarla y traerla desde la base de datos.

---
+++ BackEnd/src/main/java/com/portfolio/zarate/Repository/IPersonaRepository.java
@@ -0,0 +1,10 @@
+package com.portfolio.zarate.Repository;
+
+import com.portfolio.zarate.Entity.Persona;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IPersonaRepository extends JpaRepository<Persona,Long> {
+    
+}
