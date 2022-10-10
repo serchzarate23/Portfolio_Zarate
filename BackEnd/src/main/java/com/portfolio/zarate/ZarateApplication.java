c3543c053de3473de0c5d81374713cb57f81f397
Sergio <sergiozarate1991@hotmail.com>
2022-10-10 13:37:11 -0300

    Creación de BackEnd

---
+++ BackEnd/src/main/java/com/portfolio/zarate/ZarateApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.zarate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZarateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZarateApplication.class, args);
+	}
+
+}
---
+++ BackEnd/src/test/java/com/portfolio/zarate/ZarateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.zarate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZarateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
