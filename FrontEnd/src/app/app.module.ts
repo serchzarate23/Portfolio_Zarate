9120b17c99c5bf812c65fc049a214fbf91d1a29e
Sergio <sergiozarate1991@hotmail.com>
2022-10-11 14:30:16 -0300

    Agregamos Editar Experiencia
    Pusimos el botón de Editar y Eliminar en la sección de experiencias laborales y los completamos.

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -18,6 +18,9 @@ import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { interceptorProvider } from './service/interceptor-service';
+import { NewExperienciaComponent } from './components/experiencia/new-experiencia.component';
+import { EditExperienciaComponent } from './components/experiencia/edit-experiencia.component';
+
 
 @NgModule({
   declarations: [
@@ -33,6 +36,8 @@ import { interceptorProvider } from './service/interceptor-service';
     FooterComponent,
     HomeComponent,
     LoginComponent,
+    NewExperienciaComponent,
+    EditExperienciaComponent,
   ],
   imports: [
     BrowserModule,
